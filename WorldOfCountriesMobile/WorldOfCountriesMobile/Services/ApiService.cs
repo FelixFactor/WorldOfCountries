3184fec692138959cf9a20d82b2d561001b8959d
Joao Felix <Joao.Oliveira.Felix@formandos.cinel.pt>
2020-11-19 12:47:53 +0000

    API Service Init

---
+++ WorldOfCountriesMobile/WorldOfCountriesMobile/Services/ApiService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+namespace WorldOfCountriesMobile.Prism.Services
+{
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+
+    using WorldOfCountriesMobile.Prism.Models;
+
+    public class ApiService : IApiService
+    {
+        private protected string urlBase = App.Current.Resources["UrlAPI"].ToString();
+        private protected string servicePrefix = "/rest/v2/";
+        public async Task<Response> GetListAsync<T>(string controller)
+        {
+            try
+            {
+                HttpClient client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase),
+                };
+
+                string url = $"{servicePrefix}{controller}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        Success = false,
+                        Message = response.ReasonPhrase,
+                    };
+                }
+                
+                //defines options so null values don't return errors
+                var options = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, MissingMemberHandling = MissingMemberHandling.Ignore };
+                var list = JsonConvert.DeserializeObject<List<CountryResponse>>(await response.Content.ReadAsStringAsync(), options);
+                return new Response
+                {
+                    Success = true,
+                    Result = list
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
---
+++ WorldOfCountriesMobile/WorldOfCountriesMobile/Services/IApiService.cs
@@ -0,0 +1,11 @@
+﻿namespace WorldOfCountriesMobile.Prism.Services
+{
+    using System.Threading.Tasks;
+
+    using WorldOfCountriesMobile.Prism.Models;
+
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string controller);
+    }
+}
